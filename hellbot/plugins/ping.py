1ff7ec26b1e021721602a85478637fffb8301879
Copycat-izz-opp <85601169+Copycat-izz-opp@users.noreply.github.com>
2022-01-27 22:46:18 +0530

    Update ping.py

--- hellbot/plugins/ping.py
+++ hellbot/plugins/ping.py
@@ -22,3 +22,28 @@ def up_time(seconds: int) -> str:
 
     for x in range(len(time_list)):
         time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+@bot.on(admin_cmd(pattern="ping$"))
+async def ping(event):
+      await event.edit("`·.·★ ℘ıŋɠ ★·.·´")
+      x = await bot.get_me()
+      name = x.first_name 
+      idd = x.id
+      mention = f"[{name}](tg://user?id={idd})"
+      StartTime = time.time()
+      uptime = up_time(time.time() - StartTime)
+      start = datetime.datetime.now()
+      end = datetime.datetime.now()
+      speed = (end - start).microseconds / 1000
+      await event.edit(f"""**╰•★ఌ︎  ℘ꨄ︎ŋɠ  ఌ︎★•╯**
+   
+    __ʂ℘ɛɛɖ :__ `{speed}`
+    __ơῳŋɛཞ :__ {mention}
+""")
